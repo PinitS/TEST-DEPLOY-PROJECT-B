8ca5e2d8e939d646d356d57b54c50c91b2955346
pinit <zproject.4.u@gmail.com>
2024-09-07 13:31:33 +0700

    git init

--- src/routes/v1/index.js
+++
@@ -1,6 +0,0 @@
-const exampleRoutes = require('@routes/v1/exampleRoutes');
-const { Router } = require('express');
-const router = Router();
-
-router.use('/example', exampleRoutes);
-module.exports = router;
